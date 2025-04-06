4c3dd193db7e12130b0ac3b3a6f27b1aa270c01a
김휘래 <hramst0618@gmail.com>
2025-04-06 16:11:56 +0900

    [Bronze II] Title: 수 정렬하기, Time: 140 ms, Memory: 14776 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,19 +1,27 @@
-import java.io.*;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-        for(int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-        Arrays.sort(arr);
+
+        // 문제에서 N은 1000이하의 값을 가지지만, 이는 절댓값 기준이기에 2000이 필요하다.
+        // 나머지 1은 0자리를 의미한다.
+        boolean[] arr = new boolean[2001];
 
         for(int i = 0; i < N; i++) {
-            System.out.println(arr[i]);
+            arr[Integer.parseInt(br.readLine()) + 1000] = true;
+        }
+
+        for(int i = 0; i < 2001 ; i++) {
+            if(arr[i]) {
+                System.out.println(i - 1000);
+            }
         }
     }
 }
