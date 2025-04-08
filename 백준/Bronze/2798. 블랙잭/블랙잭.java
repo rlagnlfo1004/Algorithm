fe2376a0056c1f7ba5e2bc66eea59f8f08b6f4b1
김휘래 <hramst0618@gmail.com>
2025-04-08 11:58:39 +0900

    [Bronze II] Title: 블랙잭, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 2798                              :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: hrkim2001 <boj.kr/u/hrkim2001>              +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/2798                           #+#        #+#      #+#    */
+/*   Solved: 2025/04/08 11:15:29 by hrkim2001     ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int result = 0;
+        for(int i = 0; i < N - 2; i++) {
+            for(int j = i + 1; j < N - 1; j++) {
+                for(int k = j + 1; k < N; k++) {
+                    int sum = arr[i] + arr[j] + arr[k];
+                    if(sum > result && sum <= M)
+                        result = sum;
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
