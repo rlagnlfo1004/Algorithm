5829045c2201682a89241db06237cdeb1786d9e7
김휘래 <hramst0618@gmail.com>
2025-07-27 22:37:14 +0900

    [Gold IV] Title: 도서관, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1461. 도서관/도서관.py
+++ 백준/Gold/1461. 도서관/도서관.py
@@ -1,44 +1,24 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 1416                              :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: hrkim2001 <boj.kr/u/hrkim2001>              +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/1416                           #+#        #+#      #+#     #
-#    Solved: 2025/07/27 21:59:49 by hrkim2001     ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
 import sys
 input = sys.stdin.readline
 
 def add_to_distance(distance, arr, m):
     for i in range(len(arr) // m):
-        distance.append(arr[i * m])
+        distance.append(abs(arr[i * m]))
     
     if len(arr) % m > 0:
-        distance.append(arr[(len(arr) // m) * m])
+        distance.append(abs(arr[(len(arr) // m) * m]))
     
 
 n, m = map(int, input().split())
-left = []
-right = []
-distance = []
-numbers = list(map(int, input().split()))
+books = list(map(int, input().split()))
 
-for i in numbers:
-    if i > 0:
-        left.append(abs(i))
-    else:
-        right.append(abs(i))
+right = [x for x in books if x > 0]
+left = [x for x in books if x < 0]
 
-left.sort(reverse=True)
+left.sort()
 right.sort(reverse=True)
+distance = []
 add_to_distance(distance, left, m)
 add_to_distance(distance, right, m)
 
-distance.sort()
-result = distance.pop()
-result += sum(distance) * 2
-print(result)
-
+print(sum(distance) * 2 - max(distance))
