3d86fdffca8441c335d3f8f022075118618809d8
김휘래 <hramst0618@gmail.com>
2025-09-15 18:32:57 +0900

    [Bronze II] Title: 수 정렬하기, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -14,10 +14,40 @@ arr = [0]*N
 for i in range(N):
     arr[i] = int(input())
 
-for i in range(1, N):
-    for j in range(N - i):
-        if arr[j] > arr[j + 1]:
-            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+def merge_sort(s,e):
+    if s < e:
+        m = (s+e) // 2
+        merge_sort(s, m)
+        merge_sort(m+1, e)
+    
+        return merge(s, e)
 
+def merge(s, e):
+    global arr
+    
+    m = (s+e) // 2
+    temp_arr = []
+    l_index, r_index = s, m+1
+    
+    while l_index <= m and r_index <= e:
+        if arr[l_index] < arr[r_index]:
+            temp_arr.append(arr[l_index])
+            l_index += 1
+        else:
+            temp_arr.append(arr[r_index])
+            r_index += 1
+    
+    while l_index <= m:
+        temp_arr.append(arr[l_index])
+        l_index += 1
+    
+    while r_index <= e:
+        temp_arr.append(arr[r_index])
+        r_index += 1
+    
+    arr[s:e+1] = temp_arr
+
+
+merge_sort(0, N-1)
 for i in range(N):
     print(arr[i])
