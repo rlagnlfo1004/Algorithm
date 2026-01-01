6e4b77c5b7bc14c5bfaee929cb473594bbeca465
김휘래 <hramst0618@gmail.com>
2026-01-01 13:04:45 +0900

    [Silver IV] Title: 주몽, Time: 168 ms, Memory: 16412 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -6,45 +6,42 @@
 /*   By: hrkim2001 <boj.kr/u/hrkim2001>              +#+    +#+          +#+  */
 /*                                                  +#+      +#+        +#+   */
 /*   https://boj.kr/1940                           #+#        #+#      #+#    */
-/*   Solved: 2025/04/13 00:16:50 by hrkim2001     ###          ###   ##.kr    */
+/*   Solved: 2026/01/01 12:56:23 by hrkim2001     ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
-import java.util.*;
-import java.io.*;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main (String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
 
+        int[] A = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] arr = new int[N];
-        for(int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for(int i=0; i<N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
         }
-        br.close();
-        Arrays.sort(arr);
+        Arrays.sort(A);
 
-        int sum = 0;
-        int count = 0;
         int i = 0;
-        int j = N - 1;
-
+        int j = N-1;
+        int count = 0;
         while(i < j) {
-            sum = arr[i] + arr[j];
-            if(sum < M) {
+            int sum = A[i] + A[j];
+            if(sum == M) {
+                count++;
                 i++;
             } else if(sum > M) {
                 j--;
             } else {
-                count++;
                 i++;
-                j--;
             }
         }
-
         System.out.println(count);
     }
 }
