9c5e7615df25dd310a0dd85915114dd153c8b814
김휘래 <hramst0618@gmail.com>
2026-01-28 16:26:29 +0900

    [Silver V] Title: 다리 놓기, Time: 188 ms, Memory: 18524 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,19 +1,37 @@
-import java.io.*;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 1010                              :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: hrkim2001 <boj.kr/u/hrkim2001>              +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/1010                           #+#        #+#      #+#    */
+/*   Solved: 2026/01/28 16:21:25 by hrkim2001     ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		for(int t=0;t<T;t++) {
-			int N = sc.nextInt();
-			int M = sc.nextInt();
-			int result = 1;
-			for(int i=1;i<=N;i++) {
-				result = result * (M-i+1) / i;
-			}
-			System.out.println(result);
-		}
-		
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] D = new int[30][30];
+        for (int i = 1; i < 30; i++) {
+            D[i][i] = 1;
+            D[i][1] = i;
+        }
+
+        for (int i = 3; i < 30; i++) {
+            for (int j = 2; j < i; j++) {
+                D[i][j] = D[i - 1][j] + D[i - 1][j - 1];
+            }
+        }
+
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            System.out.println(D[M][N]);
+        }
+    }
 }
