34641e982c0c1a279b7bae46d01055607485640e
김휘래 <hramst0618@gmail.com>
2026-02-13 13:25:10 +0900

    [level 2] Title: 타겟 넘버, Time: 6.37 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,27 +1,17 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] numbers, int target) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(numbers[0]);
-        q.add(-1 * numbers[0]);
-        
-        for(int i = 1; i < numbers.length; i++) {
-            Queue<Integer> temp = new LinkedList<>();
-            while(!q.isEmpty()) {
-                int num = q.poll();
-                temp.add(num + numbers[i]);
-                temp.add(num - numbers[i]);
-            }
-            q = temp;
-        }
-        
         int answer = 0;
-        while(!q.isEmpty()) {
-            int num = q.poll();
-            if(num == target)
-                answer++;
-        }
+        answer = dfs(numbers, 0, 0, target);
         return answer;
+    }
+    
+    int dfs(int[] numbers, int n, int sum, int target) {
+        if(n == numbers.length) {
+            if(sum == target) return 1;
+            else return 0;
+        } else {
+            return dfs(numbers, n + 1, sum + numbers[n], target) 
+                + dfs(numbers, n + 1, sum - numbers[n], target);
+        }
     }
 }
