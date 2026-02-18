cef98261e1ed4ec12361140092b733afab1e9d36
김휘래 <hramst0618@gmail.com>
2026-02-18 19:09:37 +0900

    [level 2] Title: 전화번호 목록, Time: 346.18 ms, Memory: 99.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,17 +1,31 @@
-import java.util.*;
+// import java.util.*;
+
+// class Solution {
+//     public boolean solution(String[] phone_book) {
+//         HashSet<String> set = new HashSet<>();
+//         for(String str : phone_book)
+//             set.add(str);
+        
+//         for(int i = 0; i < phone_book.length; i++) {
+//             for(int j = 1; j < phone_book[i].length(); j++) {
+//                 if(set.contains(phone_book[i].substring(0, j)))
+//                     return false;
+//             }
+//         }
+        
+//         return true;
+//     }
+// }
+
+import java.util.Arrays;
 
 class Solution {
-    public boolean solution(String[] phone_book) {
-        HashSet<String> set = new HashSet<>();
-        for(String str : phone_book)
-            set.add(str);
-        
-        for(int i = 0; i < phone_book.length; i++) {
-            for(int j = 1; j < phone_book[i].length(); j++) {
-                if(set.contains(phone_book[i].substring(0, j)))
-                    return false;
-            }
-        }
+    public boolean solution(String[] phoneBook) {
+        Arrays.sort(phoneBook);
+
+        for (int i = 0; i < phoneBook.length - 1; i++)
+            if (phoneBook[i + 1].startsWith(phoneBook[i]))
+                return false;
         
         return true;
     }
