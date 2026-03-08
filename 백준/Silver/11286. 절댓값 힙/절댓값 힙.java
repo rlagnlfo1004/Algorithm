cf7b29b55a2d895ef12cb59b7e4ad6fe17e8c3aa
김휘래 <hramst0618@gmail.com>
2026-03-08 23:58:53 +0900

    [Silver I] Title: 절댓값 힙, Time: 304 ms, Memory: 26140 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -6,7 +6,7 @@
 /*   By: hrkim2001 <boj.kr/u/hrkim2001>              +#+    +#+          +#+  */
 /*                                                  +#+      +#+        +#+   */
 /*   https://boj.kr/11286                          #+#        #+#      #+#    */
-/*   Solved: 2026/01/04 14:40:17 by hrkim2001     ###          ###   ##.kr    */
+/*   Solved: 2026/03/08 23:47:56 by hrkim2001     ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
 import java.util.*;
@@ -15,33 +15,29 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
         int N = Integer.parseInt(br.readLine());
-
-        PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> {
-            int firstAbs = Math.abs(o1);
-            int secondAbs = Math.abs(o2);
-            if(firstAbs == secondAbs) {
-                return o1 - o2;
+        PriorityQueue<Integer> Q = new PriorityQueue<>((o1, o2) -> {
+            if(Math.abs(o1) == Math.abs(o2)) {
+                return o1 < o2 ? -1 : 1;
             } else {
-                return firstAbs - secondAbs;
+                return Math.abs(o1) < Math.abs(o2) ? -1 : 1;
             }
         });
 
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for (int i = 0; i < N; i++) {
-            int x = Integer.parseInt(br.readLine());
-            if(x == 0) {
-                if(queue.isEmpty()) {
-                    bw.write(0 + "\n");
-                } else {
-                    bw.write(queue.poll() + "\n");
-                }
-            } else {
-                queue.add(x);
+        for(int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if(num == 0) {
+                if(Q.isEmpty()) bw.write("0\n");
+                else bw.write(Q.poll() + "\n");
+            } else  {
+                Q.add(num);
             }
         }
-
+        
         bw.flush();
+        bw.close();
         br.close();
     }
 }
