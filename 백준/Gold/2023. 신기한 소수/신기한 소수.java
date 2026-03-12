59c7f92a6905fccbc4e32b8eafe2930b6548703c
김휘래 <hramst0618@gmail.com>
2026-03-13 00:05:01 +0900

    [Gold V] Title: 신기한 소수, Time: 480 ms, Memory: 16752 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -6,49 +6,43 @@
 /*   By: hrkim2001 <boj.kr/u/hrkim2001>              +#+    +#+          +#+  */
 /*                                                  +#+      +#+        +#+   */
 /*   https://boj.kr/2023                           #+#        #+#      #+#    */
-/*   Solved: 2026/01/07 14:15:15 by hrkim2001     ###          ###   ##.kr    */
+/*   Solved: 2026/03/12 23:35:46 by hrkim2001     ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
 import java.util.*;
 import java.io.*;
 
 public class Main {
+    
+    static int[] dx = {1, 3, 7, 9};
+    static int[] sx = {2, 3, 5, 7};
     static int N;
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedWriter bw;
+
 
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        N = Integer.parseInt(br.readLine());
 
-        DFS(2, 1);
-        DFS(3, 1);
-        DFS(5, 1);
-        DFS(7, 1);
+        for(int x : sx) {
+            isPrime(x, 1);
+        }
         bw.flush();
     }
 
-    static void DFS(int now, int j) throws IOException {
-        if(j == N) {
-            if(isPrime(now)) {
-                bw.write(now + "\n");
-                return;
-            }
+    static void isPrime(int n, int j) throws IOException {
+        for(int i = 2; i <= n / 2; i++) {
+            if(n % i == 0) return;
         }
 
-        for (int i = 1; i < 10; i += 2) {
-            int num = now * 10 + i;
-            if(isPrime(num)) {
-                DFS(num, j +  1);
+        if(j != N) {
+            for(int x : dx) {
+                isPrime(n * 10 + x, j + 1);
             }
+        } else {
+            bw.write(n + "\n");
         }
-    }
-
-    static boolean isPrime(int num) {
-        for (int i = 2; i <= num / 2; i++) {
-            if (num % i == 0) {
-                return false;
-            }
-        }
-        return true;
     }
 }
