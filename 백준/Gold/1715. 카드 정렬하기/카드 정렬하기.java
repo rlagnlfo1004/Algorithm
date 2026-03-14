7900b47896b1b09f9254f5ba358fba9fd029cb2e
김휘래 <hramst0618@gmail.com>
2026-03-14 22:09:53 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 336 ms, Memory: 25728 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -6,28 +6,29 @@
 /*   By: hrkim2001 <boj.kr/u/hrkim2001>              +#+    +#+          +#+  */
 /*                                                  +#+      +#+        +#+   */
 /*   https://boj.kr/1715                           #+#        #+#      #+#    */
-/*   Solved: 2026/01/09 16:13:53 by hrkim2001     ###          ###   ##.kr    */
+/*   Solved: 2026/03/14 21:57:35 by hrkim2001     ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = sc.nextInt();
-        PriorityQueue<Integer> q = new PriorityQueue<>();
-        for (int i = 0; i < N; i++) {
-            q.add(sc.nextInt());
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Long> Q = new PriorityQueue<>();
+        for(int i = 0; i < N; i++) {
+            Q.add(Long.parseLong(br.readLine()));
         }
 
-        int count = 0;
-        while (q.size() >= 2) {
-            int a = q.poll();
-            int b = q.poll();
-            count += a + b;
-            q.add(a + b);
+        long result = 0;
+        while(Q.size() > 1) {
+            long temp = Q.poll() + Q.poll();
+            Q.add(temp);
+            result += temp;
         }
-        System.out.println(count);
+        System.out.println(result);
+        br.close();
     }
 }
