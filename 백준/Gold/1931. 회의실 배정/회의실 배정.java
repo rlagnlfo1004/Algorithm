1a87d08e2aa8bdce67a6e9430d1255d6e4b1810e
김휘래 <hramst0618@gmail.com>
2026-03-14 23:13:07 +0900

    [Gold V] Title: 회의실 배정, Time: 420 ms, Memory: 41772 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -6,7 +6,7 @@
 /*   By: hrkim2001 <boj.kr/u/hrkim2001>              +#+    +#+          +#+  */
 /*                                                  +#+      +#+        +#+   */
 /*   https://boj.kr/1931                           #+#        #+#      #+#    */
-/*   Solved: 2026/01/11 00:30:26 by hrkim2001     ###          ###   ##.kr    */
+/*   Solved: 2026/03/14 22:33:06 by hrkim2001     ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
 import java.util.*;
@@ -15,35 +15,45 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
         
-        int[][] A = new int[N][2];
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Node> q = new PriorityQueue<>();
+
+        int count = 1;
         StringTokenizer st;
-        for (int i = 0; i < N; i++) {
+
+        for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            A[i][0] = Integer.parseInt(st.nextToken());
-            A[i][1] = Integer.parseInt(st.nextToken());
+            q.add(new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
         }
 
-        Arrays.sort(A, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] s, int[] e) {
-                if(s[1] == e[1]) { // 종료 시간 동일
-                    return s[0] - e[0];
-                } else {
-                    return s[1] - e[1];
-                }
-            }
-        });
+        int now = q.poll().e;
 
-        int count = 0;
-        int time = 0;
-        for (int i = 0; i < N; i++) {
-            if (A[i][0] >= time) {
+        while(!q.isEmpty()) {
+            Node node = q.poll();
+            if(node.s >= now) {
+                now = node.e;
                 count++;
-                time = A[i][1];
             }
         }
+    
         System.out.println(count);
+        br.close();
+    }
+
+    static class Node implements Comparable<Node> {
+        int s;
+        int e;
+
+        public Node(int s, int e) {
+            this.s = s;
+            this.e = e;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            if(this.e == o.e) return this.s - o.s;
+            else return this.e - o.e;
+        }
     }
 }
