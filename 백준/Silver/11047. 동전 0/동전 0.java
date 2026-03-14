db23e6b2f8fed133def2f2387759fc4443012d40
김휘래 <hramst0618@gmail.com>
2026-03-14 21:50:48 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -6,31 +6,37 @@
 /*   By: hrkim2001 <boj.kr/u/hrkim2001>              +#+    +#+          +#+  */
 /*                                                  +#+      +#+        +#+   */
 /*   https://boj.kr/11047                          #+#        #+#      #+#    */
-/*   Solved: 2026/01/09 16:00:41 by hrkim2001     ###          ###   ##.kr    */
+/*   Solved: 2026/03/14 21:34:09 by hrkim2001     ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int K = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
         int[] A = new int[N];
-        for (int i = 0; i < N; i++) {
-            A[i] = sc.nextInt();
+        for(int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(br.readLine());
         }
 
-        int i = N-1;
-        int count = 0;
-        while (K != 0) {
-            if (A[i] <= K) {
-                count += K / A[i];
-                K %= A[i];
+        int result = 0;
+        int sum = K;
+        int i = N - 1;
+
+        while(sum > 0) {
+            while(A[i] > sum) {
+                i--;
             }
-            i--;
+            int temp = sum / A[i];
+            result += temp;
+            sum -= temp * A[i];
         }
-        System.out.println(count);
+        System.out.println(result);
+        br.close();
     }
 }
