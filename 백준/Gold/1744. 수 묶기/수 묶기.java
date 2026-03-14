bd53fba8bc5a4666fec4da63061fc073206997fe
김휘래 <hramst0618@gmail.com>
2026-03-14 22:30:08 +0900

    [Gold IV] Title: 수 묶기, Time: 100 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.java
+++ 백준/Gold/1744. 수 묶기/수 묶기.java
@@ -6,53 +6,64 @@
 /*   By: hrkim2001 <boj.kr/u/hrkim2001>              +#+    +#+          +#+  */
 /*                                                  +#+      +#+        +#+   */
 /*   https://boj.kr/1744                           #+#        #+#      #+#    */
-/*   Solved: 2026/01/09 16:56:35 by hrkim2001     ###          ###   ##.kr    */
+/*   Solved: 2026/03/14 22:10:34 by hrkim2001     ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        PriorityQueue<Integer> plusQueue = new PriorityQueue<>(Collections.reverseOrder());
-        PriorityQueue<Integer> minusQueue = new PriorityQueue<>();
-        int zeroCount = 0;
-        for (int i = 0; i < N; i++) {
-            int num = sc.nextInt();
+        PriorityQueue<Integer> plus = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> minus = new PriorityQueue<>();
+        int zero = 0;
 
-            if (num > 0) plusQueue.add(num);
-            else if (num < 0) minusQueue.add(num);
-            else zeroCount++;
+        for(int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if(num > 0) {
+                plus.add(num);
+            } else if(num < 0) {
+                minus.add(num);
+            } else {
+                zero++;
+            }
         }
         
         int result = 0;
-        while(plusQueue.size() >= 2) {
-            int a = plusQueue.poll();
-            int b = plusQueue.poll();
+        while(plus.size() > 1) {
+            int a = plus.poll();
+            int b = plus.poll();
 
-            if(a == 1 || b == 1) result += a + b;
-            else result += a * b;
-        }
-        while(!plusQueue.isEmpty()) {
-            result += plusQueue.poll();
-        }
-        
-        while(minusQueue.size() >= 2) {
-            int a = minusQueue.poll();
-            int b = minusQueue.poll();
-
-            result += a * b;
-        }
-        while(!minusQueue.isEmpty()) {
-            if(zeroCount != 0) {
-                minusQueue.poll();
+            if(b == 1) {
+                result += a;
+                result += b;
+                break;
             } else {
-                result += minusQueue.poll();
+                result += a * b;
             }
         }
 
+        while(!plus.isEmpty()) {
+            result += plus.poll();
+        }
+
+        while(minus.size() > 1) {
+            result += minus.poll() * minus.poll();
+        }
+
+        while(!minus.isEmpty() && zero > 0) {
+            minus.poll();
+            zero--;
+        }
+
+        while(!minus.isEmpty()) {
+            result += minus.poll();
+        }
+
         System.out.println(result);
+        br.close();
     }
 }
