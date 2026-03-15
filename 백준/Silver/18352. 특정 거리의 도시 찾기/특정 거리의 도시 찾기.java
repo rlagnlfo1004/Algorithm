e2cddbe04a085c3e628739acb826cc0c7e746e2a
김휘래 <hramst0618@gmail.com>
2026-03-15 20:35:30 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1160 ms, Memory: 274264 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -6,67 +6,53 @@
 /*   By: hrkim2001 <boj.kr/u/hrkim2001>              +#+    +#+          +#+  */
 /*                                                  +#+      +#+        +#+   */
 /*   https://boj.kr/18352                          #+#        #+#      #+#    */
-/*   Solved: 2026/01/15 15:37:59 by hrkim2001     ###          ###   ##.kr    */
+/*   Solved: 2026/03/15 20:20:12 by hrkim2001     ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    static int[] visited;
-    static ArrayList<Integer>[] A;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int X = Integer.parseInt(st.nextToken()) - 1;
+        int X = Integer.parseInt(st.nextToken());
 
-        A = new ArrayList[N];
-        for (int i = 0; i < N; i++) {
-            A[i] = new ArrayList<>();
-        }
 
-        visited = new int[N];
-        for (int i = 0; i < N; i++) {
-            visited[i] = -1;
-        }
+        ArrayList<Integer>[] A = new ArrayList[N + 1];
+        for(int i = 1; i <= N; i++) A[i] = new ArrayList<>();
 
-        for (int i = 0; i < M; i++) {
+        for(int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken()) - 1;
-            int b = Integer.parseInt(st.nextToken()) - 1;
-            A[a].add(b);
+            A[Integer.parseInt(st.nextToken())].add(Integer.parseInt(st.nextToken()));
         }
-        
-        BFS(X);
-        boolean exist = false;
-        for (int i = 0; i < N; i++) {
-            if(visited[i] == K) {
-                System.out.println(i + 1);
-                exist = true;
-            }
-        }
-        if (!exist) System.out.println(-1);
-    }
 
-    static void BFS(int x) {
-        Queue<Integer> q = new LinkedList<>();
-        visited[x] = 0;
-        q.add(x);
+        int[] visited = new int[N + 1];
+        Queue<Integer> Q = new LinkedList<>();
 
-        while(!q.isEmpty()) {
-            int v = q.poll();
-            for (int node : A[v]) {
-                if(visited[node] == -1) {
-                    visited[node] = visited[v] + 1;
-                    q.add(node);
+        Q.add(X);
+        while(!Q.isEmpty()) {
+            int now = Q.poll();
+
+            for(int next : A[now]) {
+                if(visited[next] == 0 && next != X) {
+                    Q.add(next);
+                    visited[next] = visited[now] + 1;
                 }
             }
         }
+
+        boolean result = false;
+        for(int i = 1; i <= N; i++) {
+            if(visited[i] == K) {
+                System.out.println(i);
+                result = true;
+            }
+        }
+        if(!result) System.out.println(-1);
+        br.close();
     }
 }
