14a7e26322803fb4376f642c7504a5f29783d7fc
김휘래 <hramst0618@gmail.com>
2026-03-17 23:29:09 +0900

    [Gold V] Title: 집합의 표현, Time: 592 ms, Memory: 53932 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -6,60 +6,52 @@
 /*   By: hrkim2001 <boj.kr/u/hrkim2001>              +#+    +#+          +#+  */
 /*                                                  +#+      +#+        +#+   */
 /*   https://boj.kr/1717                           #+#        #+#      #+#    */
-/*   Solved: 2026/01/16 14:15:06 by hrkim2001     ###          ###   ##.kr    */
+/*   Solved: 2026/03/17 23:16:58 by hrkim2001     ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
 import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int[] A;
+
+    static int[] parent;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        A = new int[N+1];
-        for (int i = 0; i <= N; i++) {
-            A[i] = i;
-        }
+        parent = new int[n + 1];
+        for(int i = 0; i <= n; i++) parent[i] = i;
 
-        for (int i = 0; i < M; i++) {
+        for(int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int f = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            if(f == 0) { // union
-                int A = find(a);
-                int B = find(b);
-                union(A, B);
-            } else { // find -> 출력
-                int A = find(a);
-                int B = find(b);
-
-                if(A == B) bw.write("YES\n");
+            if(x == 0) union(a, b);
+            else if(x == 1) {
+                if(find(a) == find(b)) bw.write("YES\n");
                 else bw.write("NO\n");
             }
         }
-
         bw.flush();
         bw.close();
         br.close();
     }
 
-    static int find(int i) { // 대표 집합 반환
-        if(A[i] == i) {
-            return i;
-        } else {
-            A[i] = find(A[i]);
-            return A[i];
-        }
-    }
     static void union(int a, int b) {
-        A[b] = a;
+        parent[find(b)] = find(a);
+    }
+
+    static int find(int a) {
+        if(parent[a] == a) return a;
+
+        parent[a] = find(parent[a]);
+        return parent[a];
     }
 }
