b2abb69bda9e9be4522ea92895fcdd231bb2e44c
김휘래 <hramst0618@gmail.com>
2026-03-20 23:04:43 +0900

    [Gold IV] Title: 거짓말, Time: 100 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -6,78 +6,82 @@
 /*   By: hrkim2001 <boj.kr/u/hrkim2001>              +#+    +#+          +#+  */
 /*                                                  +#+      +#+        +#+   */
 /*   https://boj.kr/1043                           #+#        #+#      #+#    */
-/*   Solved: 2026/01/16 17:12:21 by hrkim2001     ###          ###   ##.kr    */
+/*   Solved: 2026/03/21 13:21:12 by hrkim2001     ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
 import java.util.*;
 import java.io.*;
 
 public class Main {
+
     static int[] parent;
-    static int[] truePeople;
-    static boolean[] findTruePeople;
-    static int[][] participant;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
         parent = new int[N + 1];
-        findTruePeople = new boolean[N + 1];
-        participant = new int[M][N + 1];
+        int[] party = new int[M + 1];
 
-        for (int i = 1; i <= N; i++) {
+        for(int i = 1; i <= N; i++) {
             parent[i] = i;
         }
+
         st = new StringTokenizer(br.readLine());
         int k = Integer.parseInt(st.nextToken());
-        truePeople = new int[k];
-        for (int i = 0; i < k; i++) {
-            truePeople[i] = Integer.parseInt(st.nextToken());
+
+        int[] li = new int[k];
+        for(int i = 0; i < k; i++) {
+            li[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < M; i++) {
+        for(int i = 1; i <= M; i++) { // 파티 입력
             st = new StringTokenizer(br.readLine());
             k = Integer.parseInt(st.nextToken());
-            for (int j = 0; j < k; j++) {
-                participant[i][j] = Integer.parseInt(st.nextToken());
-            }
 
-            for (int j = 0; j < k - 1; j++) {
-                union(participant[i][j], participant[i][j + 1]);
+            int prev = 0;
+            int next = 0;
+            for(int j = 0; j < k; j++) {
+                next = Integer.parseInt(st.nextToken());
+                if(j == 0) {
+                    party[i] = next;
+                } else {
+                    union(prev, next);
+                }
+                prev = next;
+            }
+        }
+
+        int result = M;
+        for(int i = 1; i <= M; i++) {
+            int p = find(party[i]);
+            for(int n : li) {
+                if(find(n) == p) {
+                    result--;
+                    break;
+                }
             }
         }
 
-        for (int i = 0; i < truePeople.length; i++) {
-            findTruePeople[find(truePeople[i])] = true;
-        }
+        System.out.println(result);
+        br.close();
+    }
 
-        int count = 0;
-        for (int i = 0; i < M; i++) {
-            if(!findTruePeople[find(participant[i][0])]) {
-                count++;
-            }
-        }
-        System.out.println(count);
+    static int find(int a) {
+        if(parent[a] == a) return a;
+
+        return parent[a] = find(parent[a]);
     }
 
     static void union(int a, int b) {
-        int index1 = find(a);
-        int index2 = find(b);
+        int parentA = find(a);
+        int parentB = find(b);
 
-        if(index1 != index2) {
-            parent[index2] = index1;
+        if(parentA != parentB) {
+            parent[parentB] = parentA;
         }
-    }
-
-    static int find(int x) {
-        if (parent[x] == x) {
-            return x;
-        }
-
-        return parent[x] = find(parent[x]);
     }
 }
