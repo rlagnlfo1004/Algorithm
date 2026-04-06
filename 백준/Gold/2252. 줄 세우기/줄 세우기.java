0c607a19fc4cb614a31a322fe90c4d3dbad94ff6
김휘래 <hramst0618@gmail.com>
2026-04-06 15:37:44 +0900

    [Gold III] Title: 줄 세우기, Time: 456 ms, Memory: 49404 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -6,7 +6,7 @@
 /*   By: hrkim2001 <boj.kr/u/hrkim2001>              +#+    +#+          +#+  */
 /*                                                  +#+      +#+        +#+   */
 /*   https://boj.kr/2252                           #+#        #+#      #+#    */
-/*   Solved: 2026/03/21 23:13:17 by hrkim2001     ###          ###   ##.kr    */
+/*   Solved: 2026/04/06 15:06:49 by hrkim2001     ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
 import java.util.*;
@@ -16,38 +16,42 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
-    
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-
-        ArrayList<ArrayList<Integer>> A = new ArrayList<>();
-        for(int i = 0; i <= N; i++) {
-            A.add(new ArrayList<>());
+        
+        ArrayList<Integer>[] A = new ArrayList[N + 1];
+        for(int i = 1; i <= N; i++) {
+            A[i] = new ArrayList<>();
         }
-
         int[] D = new int[N + 1];
 
         for(int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            A.get(a).add(b);
+            A[a].add(b);
             D[b]++;
         }
 
         Queue<Integer> Q = new LinkedList<>();
+
         for(int i = 1; i <= N; i++) {
-            if(D[i] == 0) Q.add(i);
+            if(D[i] == 0) {
+                Q.add(i);
+            }
         }
 
         while(!Q.isEmpty()) {
             int now = Q.poll();
             bw.write(now + " ");
 
-            for(int next : A.get(now)) {
+            for(int next : A[now]) {
                 D[next]--;
-                if(D[next] == 0) Q.add(next);
+                if(D[next] == 0) {
+                    Q.add(next);
+                }
             }
         }
 
