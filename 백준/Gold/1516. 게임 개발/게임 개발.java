a4474979cf32135c2fb3dcc3fa30acf9cdf6dac5
김휘래 <hramst0618@gmail.com>
2026-04-06 22:38:20 +0900

    [Gold III] Title: 게임 개발, Time: 232 ms, Memory: 22208 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.java
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -6,7 +6,7 @@
 /*   By: hrkim2001 <boj.kr/u/hrkim2001>              +#+    +#+          +#+  */
 /*                                                  +#+      +#+        +#+   */
 /*   https://boj.kr/1516                           #+#        #+#      #+#    */
-/*   Solved: 2026/03/21 23:52:48 by hrkim2001     ###          ###   ##.kr    */
+/*   Solved: 2026/04/06 22:14:33 by hrkim2001     ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
 import java.util.*;
@@ -19,47 +19,46 @@ public class Main {
         
         int N = Integer.parseInt(br.readLine());
 
-        int[] T = new int[N + 1];
-        int[] D = new int[N + 1];
-        int[] result = new int[N + 1];
-        
-        ArrayList<ArrayList<Integer>> A = new ArrayList<>();
-        for(int i = 0; i <= N; i++) A.add(new ArrayList<>());
-
+        ArrayList<Integer>[] A = new ArrayList[N + 1];
         for(int i = 1; i <= N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            T[i] = Integer.parseInt(st.nextToken());
-            result[i] = T[i];
+            A[i] = new ArrayList<>();
+        }
+        int[] D = new int[N + 1];
+        int[] W = new int[N + 1];
+        int[] T = new int[N + 1];
 
-            int e;
-            while((e = Integer.parseInt(st.nextToken())) != -1) {
-                A.get(e).add(i);
+        StringTokenizer st;
+        for(int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            W[i] = Integer.parseInt(st.nextToken());
+            T[i] = W[i];
+            int k;
+            while((k = Integer.parseInt(st.nextToken())) != -1) {
+                A[k].add(i);
                 D[i]++;
             }
         }
 
         Queue<Integer> Q = new LinkedList<>();
         for(int i = 1; i <= N; i++) {
-            if(D[i] == 0) {
-                Q.add(i);
-            }
-        }
-
-        while(!Q.isEmpty()) {
-            int now = Q.poll();
-            for(int next : A.get(now)) {
-                result[next] = Math.max(result[next], result[now] + T[next]);
-                D[next]--;
-
-                if(D[next] == 0) {
-                    Q.add(next);
-                }
-            }
+            if(D[i] == 0) Q.add(i);
         }
         
-        for(int i = 1; i <= N; i++) {
-            bw.write(result[i] + "\n");
+        while(!Q.isEmpty()) {
+            int now = Q.poll();
+
+            for(int next : A[now]) {
+                D[next]--;
+                T[next] = Math.max(T[next], W[next] + T[now]);
+
+                if(D[next] == 0) Q.add(next);
+            }
         }
+
+        for(int i = 1; i <= N; i++) {
+            bw.write(T[i] + "\n");
+        }
+
         bw.flush();
         bw.close();
         br.close();
