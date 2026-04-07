a3bb60c7d207df9ac81dfa62f5cf1050e56470ae
김휘래 <hramst0618@gmail.com>
2026-04-08 02:55:48 +0900

    [Gold IV] Title: 최단경로, Time: 676 ms, Memory: 109860 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -6,7 +6,7 @@
 /*   By: hrkim2001 <boj.kr/u/hrkim2001>              +#+    +#+          +#+  */
 /*                                                  +#+      +#+        +#+   */
 /*   https://boj.kr/1753                           #+#        #+#      #+#    */
-/*   Solved: 2026/03/22 19:47:52 by hrkim2001     ###          ###   ##.kr    */
+/*   Solved: 2026/04/08 02:24:01 by hrkim2001     ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
 import java.util.*;
@@ -16,67 +16,67 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
         StringTokenizer st = new StringTokenizer(br.readLine());
+        
         int V = Integer.parseInt(st.nextToken());
         int E = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(br.readLine());
 
-        boolean[] visited = new boolean[V + 1];
-        int[] distance = new int[V + 1];
-
-        for(int i = 1; i <= V; i++) distance[i] = Integer.MAX_VALUE;
-        distance[K] = 0;
-
-        ArrayList<ArrayList<Edge>> A = new ArrayList<>();
+        ArrayList<ArrayList<Node>> A = new ArrayList<>();
         for(int i = 0; i <= V; i++) A.add(new ArrayList<>());
         
+        int[] D = new int[V + 1];
+        for(int i = 1; i <= V; i++) D[i] = Integer.MAX_VALUE;
+        D[K] = 0;
+
+        boolean[] visited = new boolean[V + 1];
+        
+        PriorityQueue<Node> Q = new PriorityQueue<>();
+        Q.add(new Node(K, 0));
+
         for(int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
-            A.get(u).add(new Edge(v, w));
+            A.get(u).add(new Node(v, w));
         }
-        
-        PriorityQueue<Edge> q = new PriorityQueue<>();
-        q.add(new Edge(K, 0));
 
-        while(!q.isEmpty()) {
-            Edge now = q.poll();
+        while(!Q.isEmpty()) {
+            Node now = Q.poll();
             if(visited[now.v]) continue;
             visited[now.v] = true;
 
-            for(Edge next : A.get(now.v)) {
-                if(distance[next.v] > distance[now.v] + next.w) {
-                    distance[next.v] = distance[now.v] + next.w;
-                    q.add(new Edge(next.v, distance[next.v]));
+            for(Node next : A.get(now.v)) {
+                if(D[next.v] > D[now.v] + next.w) {
+                    D[next.v] = D[now.v] + next.w;
+                    Q.add(new Node(next.v, D[next.v]));
                 }
             }
         }
 
         for(int i = 1; i <= V; i++) {
-            if(distance[i] == Integer.MAX_VALUE) bw.write("INF\n");
-            else bw.write(distance[i] + "\n");
+            if(visited[i]) bw.write(D[i] + "\n");
+            else bw.write("INF\n");
         }
-
+        
         bw.flush();
         bw.close();
         br.close();
     }
 
-    static class Edge implements Comparable<Edge> {
+    static class Node implements Comparable<Node> {
         int v;
         int w;
 
-        public Edge(int v, int w) {
+        public Node(int v, int w) {
             this.v = v;
             this.w = w;
         }
 
         @Override
-        public int compareTo(Edge e) {
-            return this.w - e.w;
+        public int compareTo(Node o) {
+            return this.w - o.w;
         }
     }
 }
