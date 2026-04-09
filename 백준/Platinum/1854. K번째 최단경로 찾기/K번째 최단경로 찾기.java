e4059bd105fa960db9dcfa0ece69c25f7fdb0b78
김휘래 <hramst0618@gmail.com>
2026-04-10 03:41:54 +0900

    [Platinum IV] Title: K번째 최단경로 찾기, Time: 1000 ms, Memory: 101496 KB -BaekjoonHub

--- 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.java
+++ 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.java
@@ -6,7 +6,7 @@
 /*   By: hrkim2001 <boj.kr/u/hrkim2001>              +#+    +#+          +#+  */
 /*                                                  +#+      +#+        +#+   */
 /*   https://boj.kr/1854                           #+#        #+#      #+#    */
-/*   Solved: 2026/01/18 21:17:16 by hrkim2001     ###          ###   ##.kr    */
+/*   Solved: 2026/04/10 02:57:08 by hrkim2001     ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
 import java.util.*;
@@ -16,61 +16,49 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
         ArrayList<ArrayList<Node>> A = new ArrayList<>();
-        for (int i = 0; i <= N; i++) {
-            A.add(new ArrayList<>());
-        }
+        for(int i = 0; i <= n; i++) A.add(new ArrayList<>());
 
-        for (int i = 0; i < M; i++) {
+        ArrayList<PriorityQueue<Integer>> result = new ArrayList<>();
+        for(int i = 0; i <= n; i++) result.add(new PriorityQueue<>(k, (o1, o2) -> o2 - o1));
+        result.get(1).add(0);
+        
+        for(int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-
             A.get(a).add(new Node(b, c));
         }
 
-        PriorityQueue<Integer>[] distance = new PriorityQueue[N + 1];
-        Comparator<Integer> comparator = new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o2 - o1;
-            }
-        };
-        for (int i = 0; i <= N; i++) {
-            distance[i] = new PriorityQueue<>(K, comparator);
-        }
-        distance[1].add(0);
+        PriorityQueue<Node> Q = new PriorityQueue<>();
+        Q.add(new Node(1, 0));
 
-        PriorityQueue<Node> q = new PriorityQueue<>();
-        q.add(new Node(1, 0));
-
-        while(!q.isEmpty()) {
-            Node now = q.poll();
+        while(!Q.isEmpty()) {
+            Node now = Q.poll();
+            
             for(Node next : A.get(now.v)) {
-                if (distance[next.v].size() < K) {
-                    distance[next.v].add(now.c + next.c);
-                    q.add(new Node(next.v, now.c + next.c));
-                } else if(distance[next.v].peek() > now.c + next.c) {
-                    distance[next.v].poll();
-                    distance[next.v].add(now.c + next.c);
-                    q.add(new Node(next.v, now.c + next.c));
+                int weight = now.w + next.w;
+                if(result.get(next.v).size() < k) {
+                    result.get(next.v).add(weight);
+                    Q.add(new Node(next.v, weight));
+                } else if(result.get(next.v).peek() > weight) {
+                    result.get(next.v).poll();
+                    result.get(next.v).add(weight);
+                    Q.add(new Node(next.v, weight));
                 }
             }
         }
 
-        for (int i = 1; i <= N; i++) {
-            if (distance[i].size() == K) {
-                bw.write(distance[i].peek() + "\n");
-            } else {
-                bw.write(-1 + "\n");
-            }
+        for(int i = 1; i <= n; i++) {
+            if(result.get(i).size() < k) bw.write("-1\n");
+            else bw.write(result.get(i).peek() + "\n");
         }
 
         bw.flush();
@@ -79,15 +67,17 @@ public class Main {
     }
 
     static class Node implements Comparable<Node> {
-        int v, c;
-        public Node(int v, int c) {
+        int v;
+        int w;
+
+        public Node(int v, int w) {
             this.v = v;
-            this.c = c;
+            this.w = w;
         }
 
         @Override
-        public int compareTo(Node n) {
-            return this.v - n.v;
+        public int compareTo(Node o) {
+            return this.w - o.w;
         }
     }
 }
