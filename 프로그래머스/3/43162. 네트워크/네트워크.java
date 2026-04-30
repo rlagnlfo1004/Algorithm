883ef146e328ec43af78f0b6140c2fe02bedfb73
김휘래 <hramst0618@gmail.com>
2026-04-30 16:02:26 +0900

    [level 3] Title: 네트워크, Time: 0.49 ms, Memory: 91.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,46 +1,31 @@
+import java.util.*;
+
 class Solution {
-    int[] parent;
-    
     public int solution(int n, int[][] computers) {
-        parent = new int[n];
+        
+        int answer = 0;
         boolean[] visited = new boolean[n];
         
         for(int i = 0; i < n; i++) {
-            parent[i] = i;
-        }
-        
-        for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
-                if(computers[i][j] == 1)
-                    union(i, j);
-            }
-        }
-        
-        int answer = 0;
-        for(int i = 0; i < n; i++) {
-            int A = find(i);
+            if(visited[i]) continue;
             
-            if(visited[A] == false) {
-                answer++;
-                visited[A] = true;
+            Queue<Integer> Q = new LinkedList<>();
+            Q.add(i);
+            visited[i] = true;
+            answer++;
+            
+            while(!Q.isEmpty()) {
+                int now = Q.poll();
+            
+                for(int j = 0; j < n; j++) {
+                    if(computers[now][j] == 1 && j != now && !visited[j]) {
+                        Q.add(j);
+                        visited[j] = true;
+                    }
+                }
             }
         }
+        
         return answer;
-    }
-    
-    void union(int a, int b) {
-        int A = find(a);
-        int B = find(b);
-        
-        if(A != B) {
-            parent[B] = A;
-        }
-    }
-    
-    int find(int n) {
-        if(parent[n] == n)
-            return parent[n];
-        
-        return parent[n] = find(parent[n]);
     }
 }
