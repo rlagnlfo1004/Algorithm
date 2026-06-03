1f526f4a4f7505b5d1d58b18bab3f556ee7f7e15
김휘래 <hramst0618@gmail.com>
2026-06-03 14:31:34 +0900

    [level 4] Title: 도둑질, Time: 5.85 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -3,21 +3,22 @@ class Solution {
         int n = money.length;
         
         return Math.max(
-            steal(money, 0, n - 2),
-            steal(money, 1, n - 1)
+            steal(0, n - 2, money),
+            steal(1, n - 1, money)
         );
     }
     
-    private int steal(int[] money, int start, int end) {
+    private int steal(int start, int end, int[] money) {
         int prev1 = 0;
         int prev2 = 0;
+        int curr = 0;
         
         for(int i = start; i <= end; i++) {
-            int curr = Math.max(prev1, prev2 + money[i]);
+            curr = Math.max(prev1, prev2 + money[i]);
+            
             prev2 = prev1;
             prev1 = curr;
         }
-        
-        return Math.max(prev1, prev2);
+        return curr;
     }
 }
