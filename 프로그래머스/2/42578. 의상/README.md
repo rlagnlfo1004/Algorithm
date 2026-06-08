8cfb155a4c623c0e26d4dee53621a3901f3a3ff0
김휘래 <hramst0618@gmail.com>
2026-06-09 01:58:17 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 94.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=java) 
 
 ### 성능 요약
 
-메모리: 83.5 MB, 시간: 0.15 ms
+메모리: 94.3 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 18일 19:19:33
+2026년 06월 09일 01:58:13
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,15 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        HashMap<String, Integer> map = new HashMap<>();
-        for(String[] str : clothes) {
-            map.put(str[1], map.getOrDefault(str[1], 0) + 1);
+        
+        HashMap<String, Set<String>> map = new HashMap<>();
+        
+        for(String c[] : clothes) {
+            String name = c[0];
+            String type = c[1];
+            
+            map.putIfAbsent(type, new HashSet<>());
+            map.get(type).add(name);
         }
+
+        Iterator<Set<String>> iter = map.values().iterator();
         
         int answer = 1;
-        for(Integer value : map.values()) {
-            answer *= value + 1;
+        while(iter.hasNext()) {
+            Set<String> set = iter.next();
+            answer *= set.size() + 1;
         }
+        
         return answer - 1;
     }
 }
